69bffdd012c219e847cfdccf0bcc7d03df576a5f
CatalyzeAnalytics@outlook.com <your.email@example.com>
2025-06-21 22:41:17 -0400

    Updated Shiny app

--- ClassProjects/BayesianBandwidth/app/app.R
+++ ClassProjects/BayesianBandwidth/app/app.R
@@ -12,8 +12,8 @@ library(rsconnect)
 ### functions
 
 #Substract to years
-A.sub <- function(var,yr){
-  var[A2$year %in% yr]/(A2$won[A2$year %in% yr]+ A2$lost[A2$year %in% yr])
+A.sub <- function(var, yr, data = A2){
+  var[data$year %in% yr]/(data$won[data$year %in% yr]+ data$lost[data$year %in% yr])
 }
 
 #Bayesian Posterior Smoothing Parameter
@@ -24,7 +24,7 @@ h.bayes <- function(x,X,a,b){
   } ; R
 }
 
-#Kernel Density Estimation using Bayesian Bandwidth and Gausian Kernel
+#Kernel Density Estimation using Bayesian Bandwidth and Gaussian Kernel
 KDE <- function(x,h,X){
   n <- length(X); l <- length(x); R <- numeric(l)
   for (i in 1:l){
@@ -38,20 +38,11 @@ A0 <- read.csv("team_season.txt", header = 1)
 A1 <- A0[A0$leag=='N',] ; A1[A1==0] <- NA
 A2 <- A1[,c(2,4:18,34:36)]
 
-###########################################
-### Group Data By Decades
-
-g1 <- A.sub(A2$o_pts,1946:1955)
-g2 <- A.sub(A2$o_pts,1956:1965)
-g3 <- A.sub(A2$o_pts,1966:1975)
-g4 <- A.sub(A2$o_pts,1976:1985)
-g5 <- A.sub(A2$o_pts,1986:1995)
-g6 <- A.sub(A2$o_pts,1996:2005)
-
-
 ### Shiny App Codes
-x <- seq(80,140,.1)
-X <- g2
+x.start <- 60
+x.end <- 140
+
+x <- seq(x.start,x.end,.1)
 
 # ---- UI ----
 ui <- fluidPage(
@@ -61,20 +52,29 @@ ui <- fluidPage(
     sidebarPanel(
       # Input: Prior alpha0 (shape)
       numericInput("alpha", "α (shape):", 
-                   value = 2, min = 0.51, max = 10, step = 0.1),
+                   value = 1, min = 0.1, max = 5, step = 0.1),
       sliderInput("alpha_slider", NULL,
-                  min = 0.51, max = 10, value = 2, step = 0.1),
+                  min = 0.1, max = 5, value = 1, step = 0.1),
       
       # Input: Prior beta0 (scale)
       numericInput("beta", "β (scale):",
-                   value = 2, min = 0.1, max = 20, step = 0.5),
+                   value = 1, min = 0.1, max = 5, step = 0.1),
       sliderInput("beta_slider", NULL,
-                  min = 0.1, max = 20, value = 2, step = 0.5)
+                  min = 0.1, max = 5, value = 1, step = 0.1),
+      
+      # Input: Start year
+      selectInput("startyr", "Starting Year", choices = 1946:2004, selected= 1956),
+      
+      # Input: End year
+      selectInput("endyr", "Ending Year", choices = 1946:2004, selected = 1965)
     ),
     
     mainPanel(
-      h3("KDE of NBA teams PPG, 1956 - 1965 "),
-      plotlyOutput("KDEplot") 
+      h3("Distribution of NBA teams PPG in a given date range "),
+      plotlyOutput("KDEplot"),
+      
+      h3("Bayesian Posterior Bandwidth (h)"),
+      plotlyOutput("BPBplot") 
     )
   )
 )
@@ -83,7 +83,7 @@ ui <- fluidPage(
 # ---- Server Logic ----
 server <- function(input, output, session) {
   
-  # Keep slider and numericInput synced for alpha0
+  # Keep slider and numeric input synced for alpha0
   observeEvent(input$alpha, {
     updateSliderInput(session, "alpha_slider", value = input$alpha)
   })
@@ -99,21 +99,60 @@ server <- function(input, output, session) {
     updateNumericInput(session, "beta", value = input$beta_slider)
   })
   
-  # Output: Compute and display posterior mean
+  observe({
+    updateSelectInput(session, "endyr",
+                      choices = input$startyr:2004,
+                      selected = input$endyr)
+  })
+  
+  # Update X based on start/end year input
+  X <- reactive({
+    A.sub(A2$o_pts, input$startyr:input$endyr)
+  })
+  
+  X.min <- reactive({
+    floor(min(X()))
+  })
+  
+  X.max <- reactive({
+    ceiling(max(X()))
+  })
+  
+  x.seq <- reactive({
+    seq(X.min(),X.max(), .1)
+  })
+  
+  
+  # Output1: Compute and display posterior mean
   output$KDEplot <- renderPlotly({
-    x.vals <- x
-    
+    x.vals <- x.seq()
+    X.vals <- X()
     alpha <- input$alpha
     beta <- input$beta
-    h.post <- h.bayes(x.vals, X, alpha, beta)
-    density_vals <- KDE(x.vals, h.post, X)
+    h.post <- h.bayes(x.vals, X.vals, alpha, beta)
+    density_vals <- KDE(x.vals, h.post, X.vals)
     
-  plot_ly(x = x.vals, y = density_vals, type = 'scatter', mode = 'lines') %>%
+  plot_ly() %>%
+    add_lines(x = x.vals, y = density_vals, type = 'scatter', mode = 'lines', name = "KDE Curve") %>%
+    add_histogram(x = X.vals, xbins = list(start = x.start, end = x.end, size = 2), 
+                  histnorm = "probability density", name = "Histogram", marker = list(color = "pink")) %>%
+    layout(title = "",
+      xaxis = list(title = "Teams PPG", dtick = 10), yaxis = list(title = "Density"), bargap = 0.1  
+    )
+  })
+  
+  # Output2: Plot the posterior bandwidth
+  output$BPBplot <- renderPlotly({
+    x.vals <- x.seq()
+    X.vals <- X()
+    alpha <- input$alpha
+    beta <- input$beta
+    h.post <- h.bayes(x.vals, X(), alpha, beta)
+    
+    plot_ly() %>%
+      add_lines(x = x.vals, y = h.post, type = 'scatter', mode = 'lines', name = "Bayesian Posterior Bandwidth") %>%
       layout(
-        title = "",
-        xaxis = list(title = "Teams PPG"),
-        yaxis = list(title = "Density")
-      ) 
+        xaxis = list(title = "Teams PPG", dtick = 10), yaxis = list(title = "Bandwidth"))
   })
 }
 
